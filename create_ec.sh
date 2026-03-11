fb602d90fb1e1d6b45c8e7c1d64f597c1f828782
IshKevin <carterk279@gmail.com>
2026-03-11 16:39:59 +0200

    refactor

--- create_ec.sh
+++ create_ec.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 #Variable
 KEY_NAME="AutomationLabKey"
@@ -15,29 +16,39 @@ echo "Using AMI ID: $AMI_ID"
 
 # create EC2 Key pair 
 
-echo "Creating Key Pair: $KEY_NAME"
+KEY_CHECK=$(aws ec2 describe-key-pairs \
+    --key-names "$KEY_NAME" \
+    --query "KeyPairs[0].KeyName" \
+    --output text 2>/dev/null)
 
-aws ec2 create-key-pair \
-    --key-name "$KEY_NAME" \
-    --query 'KeyMaterial' \
-    --output text > "${KEY_NAME}.pem"
 
-#permission for excution
-chmod 400 "$KEY_NAME.pem"
-echo "Key pair saved to ${KEY_NAME}.pem"
+if [ "$KEY_CHECK" == "$KEY_NAME" ]; then
+    echo "Key pair $KEY_NAME already exists. Skipping creation."
+else
+    echo "Creating Key Pair: $KEY_NAME"
+    aws ec2 create-key-pair \
+        --key-name "$KEY_NAME" \
+        --query 'KeyMaterial' \
+        --output text > "${KEY_NAME}.pem"
+
+    chmod 400 "${KEY_NAME}.pem"
+    echo "Key pair saved to ${KEY_NAME}.pem"
+fi
+
 
 #Launch EC2 instance
 
-INSTANCE_INFO=$(aws ec2 run-instances \
+INSTANCE_ID=$(aws ec2 run-instances \
     --image-id "$AMI_ID" \
     --count 1 \
     --instance-type "$INSTANCE_TYPE" \
     --key-name "$KEY_NAME" \
     --tag-specifications "ResourceType=instance,Tags=[{Key=$TAG_KEY,Value=$TAG_VALUE}]" \
-    --query "Instances[0].[InstanceId,PublicIpAddress]" \
+    --query "Instances[0].InstanceId" \
     --output text)
 
-INSTANCE_ID=$(echo $INSTANCE_INFO | awk '{print $1}')
+echo "Waiting for instance to get public IP..."
+
 PUBLIC_IP=$(aws ec2 describe-instances \
     --instance-ids $INSTANCE_ID \
     --query "Reservations[0].Instances[0].PublicIpAddress" \
